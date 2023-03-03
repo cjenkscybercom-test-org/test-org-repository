ca9fcc3a8e2a9206809d2a6974430dd24248e4ad
cjenkscybercom <christina.jenks@knowit.se>
2023-03-03 10:42:11 +0100

    add org and repo variants

--- scripts/write-tag-info.sh
+++ scripts/write-tag-info.sh
@@ -7,9 +7,12 @@ echo -n '' > $TMP_FILE
 
 # Get Project info from maven
 IMAGE_OWNER="cjenkscybercom-test-org/test-org-repository"
-IMAGE_NAME="ghcr.io/${IMAGE_OWNER}/test-image"
+ORG_IMAGE_NAME="ghcr.io/cjenkscybercom-test-org/test-org-repository/test-image"
+REPO_IMAGE_NAME="ghcr.io/test-org-repository/test-image"
 IMAGE_TIMESTAMP=$(date '+%Y%m%d%H%M%S')
 
 # Write vars to the temporary file
-echo "IMAGE_TAG_LATEST=${IMAGE_NAME}:latest" >> $TMP_FILE
-echo "IMAGE_TAG_TIMESTAMP=${IMAGE_NAME}:1.0.0-${IMAGE_TIMESTAMP}" >> $TMP_FILE
+echo "IMAGE_TAG_ORG_LATEST=${ORG_IMAGE_NAME}:latest" >> $TMP_FILE
+echo "IMAGE_TAG_ORG_TIMESTAMP=${ORG_IMAGE_NAME}:1.0.0-${IMAGE_TIMESTAMP}" >> $TMP_FILE
+echo "IMAGE_TAG_REPO_LATEST=${REPO_IMAGE_NAME}:latest" >> $TMP_FILE
+echo "IMAGE_TAG_REPO_TIMESTAMP=${REPO_IMAGE_NAME}:1.0.0-${IMAGE_TIMESTAMP}" >> $TMP_FILE
